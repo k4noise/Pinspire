8c3845223720f2b057cc47f87e4dbf3b11813203
k4noise <vestertp@gmail.com>
2024-04-21 21:58:32 +0500

    init: spring initializr

---
+++ src/main/java/com/k4noise/pinspire/PinspireApplication.java
@@ -0,0 +1,13 @@
+package com.k4noise.pinspire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinspireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinspireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/k4noise/pinspire/PinspireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k4noise.pinspire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinspireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
